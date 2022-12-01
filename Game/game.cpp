c8e58e0dacf40bf2d6dd7509ce82f3ab554e1da6
galmarko1 <gal.markovich@gmail.com>
2022-12-01 15:11:31 +0200

    added spotlight

--- Game/game.cpp
+++ Game/game.cpp
@@ -6,53 +6,124 @@
 
 using namespace glm;
 
-static void printMat(const glm::mat4 mat)
-{
-	std::cout<<" matrix:"<<std::endl;
-	for (int i = 0; i < 4; i++)
-	{
-		for (int j = 0; j < 4; j++)
-			std::cout<< mat[j][i]<<" ";
-		std::cout<<std::endl;
-	}
+static void printMat(const glm::mat4 mat) {
+    std::cout << " matrix:" << std::endl;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++)
+            std::cout << mat[j][i] << " ";
+        std::cout << std::endl;
+    }
 }
 
-Game::Game() : Scene()
-{
+Game::Game() : Scene() {
 }
 
-Game::Game(float angle ,float relationWH, float near1, float far1) : Scene(angle,relationWH,near1,far1)
-{ 	
+Game::Game(float angle, float relationWH, float near1, float far1) : Scene(angle, relationWH, near1, far1) {
 }
 
 float hit_sphere(vec3 center, double radius, Ray r) {
     vec3 oc = r.origin() - center;
     float a = dot(r.direction(), r.direction());
     float b = 2.0 * dot(oc, r.direction());
-    float c = dot(oc, oc) - radius*radius;
-    float discriminant = b*b - 4*a*c;
+    float c = dot(oc, oc) - radius * radius;
+    float discriminant = b * b - 4 * a * c;
     if (discriminant < 0) //no hit
         return -1;
-    return (-b -sqrt(discriminant))/(2.0 * a);
+    return (-b - sqrt(discriminant)) / (2.0 * a);
 }
 
 
-vec3 ray_color(Ray& r){
+float hit_plane(float a, float b, float c, float d, Ray r) {
+//    vec3 normal = normalize(vec3(a, b, c));
+//    if (dot(normal, r.direction()) > 0) {
+//        return -1.0f * (dot(normal, r.origin()) + d) / dot(normal, r.direction());
+//    }
+//    return -1;
+//
+
+    vec3 normal = normalize(vec3(a, b, c));
+    vec3 center = vec3(0, -1, 0);
+    float denominator = dot(normal, r.direction());
+    if (abs(denominator) > 0.0001) {
+        vec3 diff = center - r.origin();
+        float t = dot(diff, normal) / denominator;
+        if (t > 0.0001) {
+            return t;
+        }
+    }
+    return -1;
+}
+
+vec3 ray_color_plane(Ray &r) {
+    vec4 plane = vec4(0, -0.5, 0, -1);
+    vec3 plane_color = vec3(0, 0, 1);
+    vec3 light_dir = normalize(vec3(0, -1, 0)); // light dir
+    vec3 light_color = vec3(1, 1, 1);
+    vec3 spot_light_dir = normalize(vec3(0, -1, 0)); // spotlight dir
+    vec3 spot_light_color = vec3(1, 1, 1); // spotlight
+    vec3 spot_light_origin = vec3(0, 1, -5); // spotlight
+    float diffuseK = 1;
+    float specularK = 0.7;
+    float w = 1.0;
+    float spot_factor = 1.0;
+    float t = hit_plane(plane[0], plane[1], plane[2], plane[3], r);
+    if (t > 0) {
+        vec3 normal = normalize(vec3(plane[0], plane[1], plane[2]));
+        vec3 R = light_dir - 2 * dot(light_dir, normal) * normal;
+        float p = pow(max(dot(normalize(-1.0f * r.direction()), normalize(R)), 0.0f), 256);
+        vec3 specular = vec3(0, 0, 0);
+        specular = specularK * p * light_color;
+
+        vec3 L = normalize(spot_light_dir);
+        vec3 D = normalize(r.at(t) - spot_light_origin);
+        float spotCosine = dot(D, L);
+        if (spotCosine >= 0.6) { // ?
+//            return vec3(1,1,1);
+            spot_factor = pow(spotCosine, 5);//?
+//            spot_factor = 1.0;
+        } else {
+            spot_factor = 0.0;
+        }
+
+        vec3 spot = spot_factor * vec3(1, 1, 1);
+        vec3 diffuse = (float) std::max((double) dot(normalize(light_dir), normal), 0.0) * plane_color * diffuseK;
+//        diffuse = vec3(0,0,0);
+        plane_color = diffuse + specular + spot;
+//        vec3 plane_def = diffuse + specular;
+        if (plane_color.x > 1.0f) {
+            plane_color = vec3(1.0, plane_color.y, plane_color.z);
+        }
+        if (plane_color.y > 1.0f) {
+            plane_color = vec3(plane_color.x, 1.0, plane_color.z);
+        }
+        if (plane_color.z > 1.0f) {
+            plane_color = vec3(plane_color.x, plane_color.y, 1.0f);
+        }
+        return plane_color;
+    }
+    //background
+    vec3 unit_dir = normalize(r.direction());
+    t = 0.5 * unit_dir[1] + 1.0;
+    float t2 = 1.0 - t;
+    return t2 * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+}
+
+
+vec3 ray_color(Ray &r) {
     vec3 center = vec3(0, 0, -1); // sphere center
-    vec3 sphere_color = vec3(1,0,0);
+    vec3 sphere_color = vec3(1, 0, 0);
     vec3 light_dir = normalize(vec3(0, 15, -3)); // light dir
     vec3 light_color = vec3(1, 1, 1);
     float diffuseK = 1;
     float specularK = 0.7;
 
     float t = hit_sphere(center, 0.5, r);
-    if(t > 0)
-    {
-        vec3 normal = normalize(r.at(t) - center) ;
+    if (t > 0) {
+        vec3 normal = normalize(r.at(t) - center);
         vec3 R = light_dir - 2 * dot(light_dir, normal) * normal;
-        float p = pow(max(dot(normalize(-1.0f * r.direction()), normalize(R)), 0.0f) , 256);
-        vec3 specular = vec3 (0,0,0);
-        specular = specularK * p * light_color ;
+        float p = pow(max(dot(normalize(-1.0f * r.direction()), normalize(R)), 0.0f), 256);
+        vec3 specular = vec3(0, 0, 0);
+        specular = specularK * p * light_color;
 
         vec3 diffuse = (float) std::max((double) dot(normalize(light_dir), normal), 0.0) * sphere_color * diffuseK;
         vec3 color = diffuse + specular;
@@ -62,10 +133,10 @@ vec3 ray_color(Ray& r){
     vec3 unit_dir = normalize(r.direction());
     t = 0.5 * unit_dir[1] + 1.0;
     float t2 = 1.0 - t;
-    return t2 * vec3 (1.0,1.0,1.0) + t * vec3 (0.5, 0.7, 1.0);
+    return t2 * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
 
-unsigned char* part1(){
+unsigned char *part1() {
     float mat[256][256][3];
 //    float ratio = 16.0/9.0;
 //    int width = 400;
@@ -73,29 +144,30 @@ unsigned char* part1(){
 
     vec3 origin = vec3(0, 0, 4.0);
 
-    for(int i = 0; i <256 ; i ++){
-        for(int j = 0; j < 256; j ++){
-            double u = double(j)/ 255;
-            double v = double(i)/ 255;
-            vec3 xDir = vec3(2 * u,0,0);
+    for (int y = 0; y < 256; y++) {
+        for (int x = 0; x < 256; x++) {
+            double u = double(x) / 255;
+            double v = double(y) / 255;
+            vec3 xDir = vec3(2 * u, 0, 0);
             vec3 yDir = vec3(0, 2 * v, 0);
             vec3 left_corner = vec3(-1, -1, 0);
 
             vec3 dir = left_corner + xDir + yDir - origin;
             Ray r = Ray(origin, dir);
-            vec3 color = ray_color(r);
-            mat[i-1][j-1][0] = color[0];
-            mat[i-1][j-1][1] = color[1];
-            mat[i-1][j-1][2] = color[2];
+//            vec3 color = ray_color(r);
+            vec3 color = ray_color_plane(r);
+            mat[y][x][0] = color[0];
+            mat[y][x][1] = color[1];
+            mat[y][x][2] = color[2];
 
         }
     }
 
     int index = 0;
-    unsigned char *data = new unsigned char [256 * 256 * 4];
+    unsigned char *data = new unsigned char[256 * 256 * 4];
 
-    for (int i =0; i< 256; i++){
-        for(int j =0; j < 256; j++){
+    for (int i = 0; i < 256; i++) {
+        for (int j = 0; j < 256; j++) {
             data[index] = mat[i][j][0] * 255.0;
             data[index + 1] = mat[i][j][1] * 255.0;
             data[index + 2] = mat[i][j][2] * 255.0;
@@ -106,62 +178,55 @@ unsigned char* part1(){
     return data;
 }
 
-void Game::Init()
-{		
+void Game::Init() {
 
-	AddShader("../res/shaders/pickingShader");	
-	AddShader("../res/shaders/basicShader");
+    AddShader("../res/shaders/pickingShader");
+    AddShader("../res/shaders/basicShader");
 
 //    part1();
 
 //	AddTexture("../res/textures/box0.bmp",false);
 
     unsigned char *zibi = part1();
-    AddTexture(256,256,zibi);
+    AddTexture(256, 256, zibi);
 
-	AddShape(Plane,-1,TRIANGLES);
-	
-	pickedShape = 0;
-	
-	SetShapeTex(0,0);
-	MoveCamera(0,zTranslate,10);
-	pickedShape = -1;
-	
-	//ReadPixel(); //uncomment when you are reading from the z-buffer
+    AddShape(Plane, -1, TRIANGLES);
+
+    pickedShape = 0;
+
+    SetShapeTex(0, 0);
+    MoveCamera(0, zTranslate, 10);
+    pickedShape = -1;
+
+    //ReadPixel(); //uncomment when you are reading from the z-buffer
 }
 
-void Game::Update(const glm::mat4 &MVP,const glm::mat4 &Model,const int  shaderIndx)
-{
-	Shader *s = shaders[shaderIndx];
-	int r = ((pickedShape+1) & 0x000000FF) >>  0;
-	int g = ((pickedShape+1) & 0x0000FF00) >>  8;
-	int b = ((pickedShape+1) & 0x00FF0000) >> 16;
-	s->Bind();
-	s->SetUniformMat4f("MVP", MVP);
-	s->SetUniformMat4f("Normal",Model);
-	s->SetUniform4f("lightDirection", 0.0f , 0.0f, -1.0f, 0.0f);
-	if(shaderIndx == 0)
-		s->SetUniform4f("lightColor",r/255.0f, g/255.0f, b/255.0f,1.0f);
-	else 
-		s->SetUniform4f("lightColor",0.7f,0.8f,0.1f,1.0f);
-	s->Unbind();
+void Game::Update(const glm::mat4 &MVP, const glm::mat4 &Model, const int shaderIndx) {
+    Shader *s = shaders[shaderIndx];
+    int r = ((pickedShape + 1) & 0x000000FF) >> 0;
+    int g = ((pickedShape + 1) & 0x0000FF00) >> 8;
+    int b = ((pickedShape + 1) & 0x00FF0000) >> 16;
+    s->Bind();
+    s->SetUniformMat4f("MVP", MVP);
+    s->SetUniformMat4f("Normal", Model);
+    s->SetUniform4f("lightDirection", 0.0f, 0.0f, -1.0f, 0.0f);
+    if (shaderIndx == 0)
+        s->SetUniform4f("lightColor", r / 255.0f, g / 255.0f, b / 255.0f, 1.0f);
+    else
+        s->SetUniform4f("lightColor", 0.7f, 0.8f, 0.1f, 1.0f);
+    s->Unbind();
 }
 
-void Game::WhenRotate()
-{
+void Game::WhenRotate() {
 }
 
-void Game::WhenTranslate()
-{
+void Game::WhenTranslate() {
 }
 
-void Game::Motion()
-{
-	if(isActive)
-	{
-	}
+void Game::Motion() {
+    if (isActive) {
+    }
 }
 
-Game::~Game(void)
-{
+Game::~Game(void) {
 }
